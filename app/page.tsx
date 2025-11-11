d6b8d0785ebc9c2bc552d1d858881b282c8198dd
Jorge Florentino <jorgeflorentinomarbe@gmail.com>
2025-11-11 13:11:00 +0100

    added 0debt logo

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,17 @@
 import { Button } from "@/components/ui/button";
+import Image from "next/image";
 
 export default function Home() {
   return (
     <div className="flex min-h-screen flex-col items-center justify-center p-24">
-      <Button>Hello World</Button>
+      <Image
+        src="/0debt-logo.svg"
+        alt="0debt Logo"
+        width={100}
+        height={100}
+        draggable={false}
+      />
+      <Button className="mt-2">Hello World</Button>
     </div>
 
   );
